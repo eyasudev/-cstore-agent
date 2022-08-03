a952f85605412c3859908d788cb5f9e2815e1d3d
purcellchris <mitchelljack979@gmail.com>
2022-08-03 20:17:03 +0100

    updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # cstore-agent
+
+# Steps: 
+1. Clone git repo 
+2. Install virtualenv 
+3. Install ansible 
+4. Work on the ansible file 
+	a. Pull ansible tasks repo 
+		SSH link: git@github.com:cliffordolaw/ansible-pull-sample-tak.git
+		Https link: https://github.com/cliffordolaw/ansible-pull-sample-task.git
+
+		Sample command: 
+		sudo ansible-pull -U https://github.com/cliffordolaw/ansible-pull-sample-task.git
+		
+	b. Modify the ansible yaml file accordingly (hostname, list of tasks) 
+	c. Push back to repo 
+	
+5. Modify the python app config file accordingly 
+	a. ansible-pull command and the git repo to pull ansible yaml file from 
+	b. endpoint for registering agent 
+	
+6. Update cron job "rc.local" to suit your application
--- agent.py
+++ agent.py
@@ -1,35 +1,30 @@
 #! /usr/bin/python3
 
-#git clone git@github.com:cliffordolaw/ansible-pull-sample-tak.git
-#git clone https://github.com/cliffordolaw/ansible-pull-sample-task.git
-
-# sudo ansible-pull -U https://github.com/cliffordolaw/ansible-pull-sample-task.git
-
 import requests
-import os
+import os, sys
 import re, uuid
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO) 
 
 def run():
+    logging.info("---- Starting | param ----")
     #important variables
     mac_addr = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
     endpoint = "http://192.168.8.100:5001/heartbeat"
     payload = {'mac_addr':mac_addr}
     ansible_pull_cmd = "sudo ansible-pull -U https://github.com/cliffordolaw/ansible-pull-sample-task.git"
 
-    #Send request to web server
-    resp = requests.post(endpoint, json=payload) 
-    logging.info("Server Response: {}".format(resp.text))
-    
+    #Send request to web server    
     try:
+        resp = requests.post(endpoint, json=payload, timeout=15) 
+        logging.info("Server Response: {}".format(resp.text))
         resp_json = resp.json()
     except Exception as e:
         print(e, file = sys.stderr)
         return
     
-    logging.info("Resp_json[resp]: {}".format(resp_json["resp"]))
+    #logging.info("Resp_json[resp]: {}".format(resp_json["resp"]))
     
     #Pull repo for ansible playbook if resp == 0;
     #i.e. it is a new Agent
